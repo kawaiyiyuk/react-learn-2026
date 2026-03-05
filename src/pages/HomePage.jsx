5984c56795266a016cf255de7668247cb055efa6
lqmini <399599243@qq.com>
2026-03-05 19:14:37 +0800

    day2完成

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,31 @@ import { Link } from 'react-router-dom'
 import Card from '../components/Card'
 
 function HomePage() {
-  const [tasks] = useState([
+  const [tasks, setTasks] = useState([
     { id: '1', title: '学习 useState', completed: false },
     { id: '2', title: '实现任务列表渲染', completed: true },
     { id: '3', title: '后面再实现新增 / 删除', completed: false },
   ])
-
+  const [inputValue, setInputValue] = useState('初始值')
+  console.log('渲染时 inputValue =', inputValue)
+  const handleToggleTask = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    )
+  }
+  const handleAddTask = () => {
+    const newId = crypto.randomUUID()
+    const newTask = {id:newId,title: inputValue, completed: false}
+    setTasks((prevTasks) => [...prevTasks, newTask])
+    setInputValue('')
+  }
+  const handleDeleteTask = (id) => {
+    // setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id))
+    console.log('删除任务', id)
+    setTasks((prevTasks) =>  prevTasks.filter((task) => task.id !== id))
+  }
   return (
     <div className="page">
       <h2>首页 · 待办列表</h2>
@@ -27,10 +46,27 @@ function HomePage() {
       </Card>
 
       <Card title="示例任务列表">
+        <input placeholder='请输入任务' type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <button type='button' onClick={handleAddTask}>新增</button>
         <ul>
           {tasks.map((task) => (
             <li key={task.id}>
-              {task.title}（{task.completed ? '已完成' : '未完成'}）
+              <label>
+                <input
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={() => handleToggleTask(task.id)}
+                />
+                <span style={{ marginLeft: '8px' }}>
+                  {task.title}
+                  <span className={task.completed ? 'task-status done' : 'task-status'}>
+                    （{task.completed ? '已完成' : '未完成'}）
+                  </span>
+                </span>
+                <span>
+                  <button type='button' onClick={() => handleDeleteTask(task.id)}>删除</button>
+                </span>
+              </label>
             </li>
           ))}
         </ul>
